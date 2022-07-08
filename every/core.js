6394beba6d9a77923dd0623560c8d838d0635e04
Yusra Anshoor <yusraanshoor85@gmail.com>
2022-07-08 18:22:30 +0100

    New array methods added

--- every/core.js
+++ every/core.js
@@ -1,35 +1,75 @@
 // Check to see if all elements in an array
 // are even numbers.
 
-function allEven (input) {
-  return input;
-};
+function allEven(input) {
+  return input.every((num) => {
+    return num % 2 === 0;
+  });
+}
 
 // Check to see if all elements in an array
 // are of the same type.
 
-function allSameType (input) {
-  return input;
-};
+function allSameType(input) {
+  return input.every((element) => {
+    return typeof input[0] === typeof element;
+  });
+}
 
 // Check to see if every element in the matrix is
 // an array and that every element in the array is
 // greater than 0.
 
-function positiveMatrix (input) {
-  return input;
-};
+function positiveMatrix(input) {
+  if (elementIsArray(input)) {
+    return input.every((array) => {
+      return array.every((element) => {
+        return element > 0;
+      });
+    });
+  }
+}
+function elementIsArray(matrix) {
+  return matrix.every((array) => {
+    return Array.isArray(array);
+  });
+}
 
 // Check that all items in an array are strings
 // and that they all only contain the same vowels.
 
-function allSameVowels (input) {
+function allSameVowels(input) {
+  let vowels = [a,e,i,o,u];
+  if (isString(input)) {
+    return input.every((element) => {
+     return element.every((letter){
+      let matchLetter = vowels.indexOf(letter)
+      })
+    });
+  }
   return input;
-};
+}
+
+function whatVowelsArePresent (array){
+  let 
+  let vowels = [a,e,i,o,u]
+  array.map((element) => {
+    element.map()
+    let letters = element.split('')
+     letters.map
+    
+  })
+}
+
+function isString(array) {
+  return array.every((element) => {
+    return typeof element === "string";
+  });
+}
 
 module.exports = {
   allEven,
   allSameType,
   positiveMatrix,
-  allSameVowels
+  allSameVowels,
 };
--- filter/core.js
+++ filter/core.js
@@ -1,22 +1,40 @@
-function onlyEven (array) {
+function onlyEven(array) {
   // your code here
-};
+  return array.filter((element) => {
+    return element % 2 === 0;
+  });
+}
 
-function onlyOneWord (array) {
+function onlyOneWord(array) {
   // your code here
-};
+  return array.filter((element) => {
+    return element.length === element.replaceAll(" ", "").length;
+  });
+}
 
-function positiveRowsOnly (array) {
+function positiveRowsOnly(array) {
   // your code here
-};
+  return array.filter((array) => {
+    return array.every((element) => {
+      return element > 0;
+    });
+  });
+}
 
-function allSameVowels (array) {
+function allSameVowels(array) {
   // your code here
-};
+  return array.filter((word) => {
+    let re = /[aeiou]/gi;
+    let found = word.match(re);
+    return found.every((vowel, index) => {
+      return found[0] === vowel;
+    });
+  });
+}
 
 module.exports = {
   onlyEven: onlyEven,
   onlyOneWord: onlyOneWord,
   positiveRowsOnly: positiveRowsOnly,
-  allSameVowels: allSameVowels
+  allSameVowels: allSameVowels,
 };
