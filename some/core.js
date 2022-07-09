16fb83b1d56db655576117369634344c40c98c59
Yusra Anshoor <yusraanshoor85@gmail.com>
2022-07-09 19:14:07 +0100

    added functions to some array method exercise

--- some/core.js
+++ some/core.js
@@ -1,34 +1,47 @@
 // Check to see if any of the elements in the
 // array are numbers greater than 10.
 
-function anyGreaterThan10 (input) {
-  return input;
-};
+function anyGreaterThan10(input) {
+  return input.some((element) => {
+    return element > 10;
+  });
+}
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
-function longWord (input) {
-  return input;
-};
+function longWord(input) {
+  return input.some((string) => {
+    return string.length > 10;
+  });
+}
 
 // Check to see if any of the elements in
 // the matrix are true.
 
-function truePossibilities (input) {
-  return input;
-};
+function truePossibilities(input) {
+  return input.some((realArray) => {
+    return realArray.some((element) => {
+      return element === true;
+    });
+  });
+}
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
-function lostCarcosa (input) {
-  return input;
-};
+function lostCarcosa(input) {
+  return input.some((string) => {
+    let words = string.split(" ");
+    return words.some((word) => {
+      return word === "Lost";
+    });
+  });
+}
 
 module.exports = {
   anyGreaterThan10,
   longWord,
   truePossibilities,
-  lostCarcosa
+  lostCarcosa,
 };
