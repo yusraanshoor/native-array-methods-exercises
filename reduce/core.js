68a187de3fd7bacffddebacc302627847cee4867
Yusra Anshoor <yusraanshoor85@gmail.com>
2022-07-09 17:43:34 +0100

    functions using reduce method

--- reduce/core.js
+++ reduce/core.js
@@ -1,24 +1,49 @@
-function sum (array) {
+function sum(array) {
   // your code here
+  return array.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue;
+  }, 0);
 }
 
-function productAll (array) {
+function productAll(array) {
   // your code here
+  let result = 1;
+  array.forEach((realArray) => {
+    result *= arrayProduct(realArray);
+  });
+  return result;
 }
 
-function objectify (array) {
+function arrayProduct(array) {
+  return array.reduce((previousValue, currentValue) => {
+    return previousValue * currentValue;
+  }, 1);
+}
+function objectify(array) {
   // your code here
-
+  let obj = {};
+  array.forEach((realArray) => {
+    realArray.reduce((previousValue, currentValue) => {
+      return (obj[previousValue] = currentValue);
+    });
+  });
+  return obj;
 }
 
-function luckyNumbers (array) {
+function luckyNumbers(array) {
   // your code here
+  return array.reduce((previousValue, currentValue, index) => {
+    if (index !== array.length - 1) {
+      return previousValue + currentValue + ", ";
+    } else {
+      return previousValue + "and " + currentValue;
+    }
+  }, "Your lucky numbers are: ");
 }
-
 
 module.exports = {
   sum: sum,
   productAll: productAll,
   objectify: objectify,
-  luckyNumbers: luckyNumbers
+  luckyNumbers: luckyNumbers,
 };
